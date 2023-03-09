e9b287b926d1ea6906d4b33e6189be275ed2fe57
RjKim <RjKim@DESKTOP-3LJ949P>
2023-03-09 15:33:05 -0500

    CP312, A3, Q2 completed with testing.

--- CP312/a3/main.py
+++ CP312/a3/main.py
@@ -22,82 +22,57 @@ def q1(citations):
 
     return middle if citations[middle] != 0 else -1
 
-def q2(ls):
+def recur1(ls, left, right):
     
-    def recur(ls, left, right):
-
-        # Base Case of single element subarray
-        if left == right:
-            return [[ls[left], 1]]
-        
-        middle = (left + right) // 2
-
-        # Consider both left and right sides of subarray
-        left_result = recur(ls, left, middle)
-        right_result = recur(ls, middle, right)
-        
-        # Find max instance of majority elem in left subarray
-        L = 0
-        for i in range(len(left_result)):
-            if left_result[i][1] > left_result[L][1]:
-                L = i
-            
-        # Find max instance of majority elem in right subarray
-        R = 0
-        for i in range(len(right_result)):
-            if right_result[i][1] > right_result[R][1]:
-                R = i
-        
-        # Found max instances of same GIF object on left and right sides
-        if left_result[L][0] == right_result[R][0]:
-            
-            # Combine the counts found in both sides
-            left_result[L][1] += right_result[R][1]
-            return [left_result[L]]
-        
-        # Max Instace of both sides is left and we want to combine the count from right and return to a higher level 
-        elif left_result[L][1] > right_result[R][1]:
-
-            for i in range(len(right_result)):
-                if right_result[i][0] == left_result[L][0]:
-                    left_result[L][1] += right_result[i][1]
-                    break
-            
-            return [left_result[L]]
-        
-        # Max Instance of both sides is right and we want to combine the count from left and return to a higher level
-        elif left_result[L][1] < right_result[R][1]:
-
-            for i in range(len(left_result)):
-                if left_result[i][0] == right_result[R][0]:
-                    right_result[R][1] += left_result[i][1]
-                    break
-            
-            return [right_result[R]]
-
-        # Max instances are different GIF objects but have the same occurence, therefore algo cant decide which is max at this layer
-        else:
-
-            # Combine instance count from right into left 
-            for i in range(len(right_result)):
-                if right_result[i][0] == left_result[L][0]:
-                    left_result[L][1] += right_result[i][1]
-                    break 
-            
-            # Combine instnace count from left into right
-            for i in range(len(left_result)):
-                if left_result[i][0] == right_result[R][0]:
-                    right_result[R][1] += left_result[i][1]
-                    break
-
-            return [right_result[R], left_result[L]]
-
-    potentials = recur(ls, 0, len(ls)-1) 
-
-    if len(potentials) != 1:
+    if len(ls) == 0:
         return "FAIL"
     
-    return str(potentials[0][0])
+    # Base Case
+    if left >= right:
+        return ls[left]
+    
+    # If not Base Case, Divide
+    middle = (left + right) // 2
+    l_result = recur1(ls, left, middle)
+    r_result = recur1(ls, middle+1, right)
+
+    # Base Case for the number of Instances required to be majority
+    majority_target = ((right-left) // 2) + 1
+    if majority_target == 1 and l_result == r_result:
+        return l_result
+    
+    elif majority_target == 1:
+        return "FAIL"
+    
+    # Counting the number of instance of possible majority from Left
+    l_count = 0
+    if l_result != "FAIL":
+        for i in range(left, right+1):
+            if ls[i] == l_result:
+                l_count += 1
+    
+    # Counting the number of instance of possible majority from Right
+    r_count = 0
+    if r_result != "FAIL":
+        for i in range(left, right+1):
+            if ls[i] == r_result:
+                r_count += 1 
+        
+    # Is Left possibility the majority
+    if l_count >= majority_target:
+        return l_result
+    
+    # Is Right possibility the majority
+    elif r_count >= majority_target:
+        return r_result
+
+    else:
+        return "FAIL"
+
+def q2(ls):
+
+    result = recur1(ls, 0, len(ls)-1) 
+    return result
 
 def q3():
     pass
@@ -133,6 +108,22 @@ def test_q2():
     A = ['a', '2', '2', 'c', 'v', '2', '2']
     result = q2(A)
     assert result == '2', f"Expected: 2 got {result}"
+
+    A = []
+    result = q2(A)
+    expect = "FAIL"
+    assert result == expect, f"Expected {expect} got {result}" 
+
+    A = ['1', '2', '3']
+    result = q2(A)
+    expect = "FAIL"
+    assert result == expect, f"Expected {expect} got {result}"
+
+    A = ['A', 'A', 'B', 'C', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E']
+    result = q2(A)
+    expect = "E"
+    assert result == expect, f"Expected {expect} got {result}"
+
 
 def test_q3():
     C1 = [1, 2, 3, 4, 5, 6]
