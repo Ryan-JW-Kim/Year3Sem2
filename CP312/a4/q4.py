11a4bb36f0f6425a0104c04fde845088ae53e095
Ryan Kim <rj.kim.toronto@gmail.com>
2023-03-22 20:54:59 -0400

    CP312 A4 Q4 Complete solution

--- CP312/a4/q4.py
+++ CP312/a4/q4.py
@@ -1,45 +1,54 @@
 from pandas import *
-def q4(s):
+def q4(X):
     
-    r = s[::-1]
+    # Create a reverse of the list to compare for longest common subsequence
+    Y = X[::-1]
 
-    n = len(s)
-    P = [[0 for _ in range(len(s))] for __ in range(len(s))]
-    D = [[0 for _ in range(len(s))] for __ in range(len(s))]
+    # Initialize empty matrix
+    n = len(X)
+    C = [[0 for _ in range(len(X)+1)] for __ in range(len(X)+1)]
+    D = [[0 for _ in range(len(X)+1)] for __ in range(len(X)+1)]
 
-    if s == r:
-        return len(s)
+    # Simple case check
+    if X == Y:
+        return X
 
-    for i in range(n):
-        for j in range(n):
-            if s[i] == r[j]:
-                P[i][j] = P[i-1][j-1] + 1
+    elif X == "":
+        return ""
+
+    # Filling the matrix
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            ii = i-1
+            jj = j-1
+
+            if Y[ii] == X[jj]:
+                C[i][j] = C[i-1][j-1] + 1
                 D[i][j] = "up-left"
 
+                # print(f"Found Equals: {X[i]} and {Y[j]}  {i},{j}")
+
             else:
+
+                m = max(C[i-1][j], C[i][j-1])
             
-                if P[i-1][j] > P[i][j-1]:
-                    P[i][j] = P[i-1][j]
+                if m == C[i-1][j]:
+                    C[i][j] = C[i-1][j]
                     D[i][j] = "up"
 
-                else:
-                    P[i][j] = P[i][j-1]
+                elif m == C[i][j-1]:
+                    C[i][j] = C[i][j-1]
                     D[i][j] = "left"
     
-    print('\n\n\n')
-    print(DataFrame(P, columns=list(s), index=list(r)))
-    print('\n\n\n')
-    print(DataFrame(D, columns=list(s), index=list(r)))
-    print('\n\n\n')
-
-    row = n-1
-    column = n-1
+    # Complete answer retrival via back pointers
+    row = n
+    column = n
     LCS_palindrome = ""
 
     while row > 0 and column > 0:
 
         if D[row][column] == "up-left":
-            print(s[row])
+            LCS_palindrome += Y[row-1]
             row -= 1
             column -= 1
     
@@ -56,7 +65,26 @@ def test_q4():
     test_sequence = "strabetubsa"
     real = "abeba"
     result = q4(test_sequence)
-    assert len(real) == result, f"Expected: {real} ({len(real)}) Got: {result} ({len(result)})"
+    assert len(real) == len(result), f"Expected: {real} ({len(real)}) Got: {result} ({len(result)})"
 
+    test_sequence = "babad"
+    real = "bab"
+    result = q4(test_sequence)
+    assert len(real) == len(result), f"Expected: {real} ({len(real)}) Got: {result} ({len(result)})"
+
+    test_sequence = "cbbd"
+    real = "bb"
+    result = q4(test_sequence)
+    assert len(real) == len(result), f"Expected: {real} ({len(real)}) Got: {result} ({len(result)})"
+
+    test_sequence = "racecar"
+    real = "racecar"
+    result = q4(test_sequence)
+    assert len(real) == len(result), f"Expected: {real} ({len(real)}) Got: {result} ({len(result)})"
+
+    test_sequence = "Helleworld"
+    real = "elle"
+    result = q4(test_sequence)
+    assert len(real) == len(result), f"Expected: {real} ({len(real)}) Got: {result} ({len(result)})"
 
 test_q4()
