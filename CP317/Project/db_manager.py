0b541390e87973870704ae96e99bcfb05a399f6b
Ryan Kim <rj.kim.toronto@gmail.com>
2023-04-05 18:38:53 -0400

    CP317 some database action

--- CP317/Project/db_manager.py
+++ CP317/Project/db_manager.py
@@ -48,13 +48,14 @@ class Database:
                        "Reserve Book": ["student", "staff"],
                        "Checkout Book": ["staff"]}
         
+
         return True if origin.type in authorities[cmd] else False
 
     @staticmethod
-    def execute_query(query: str):
+    def execute_query(query: str, fetch=False):
         # mycursor = Database.active_connector.cursor()
         # mycursor.execute(query)
-        # return True
+        # return mycuros.fetchall() if fetch else True
 
         print(f"\n    Executing query: {query}\n")
 
@@ -67,41 +68,107 @@ class Database:
         return True
 
     @staticmethod
-    def get_books(origin):
+    def get_book(origin):
 
-        if Database.user_has_authority(origin, "Get Books") is False:
+        if Database.user_has_authority(origin, "Get Book") is False:
             return False
         
-        params = [{"Display Name": "ID",
-                   "Type": "Number"}]
-
-        query_template = "SELECT * FROM books WHERE id=%s" 
-        query = Display.prompt_for_query(params, origin, query_template)
-
-        if query:
-            Database.execute_query(query)
+        book = Database.query_book_info(origin)
+        
+        if book:
             return True
 
         return False
     
     @staticmethod
-    def reserve_book(origin):
+    def query_book_info(origin, id=None):
+        if Database.user_has_authority(origin, "Get Book") is False:
+            return False
+        
+        if id is None:
+            params = [{"Display Name": "Book ID",
+                   "Type": "Number"}]
+            
+            query_template = "SELECT * FROM books WHERE id=%s" 
+            query = Display.prompt_for_query(params, origin, query_template)
+
+            if query:
+                return Database.execute_query(query)
+                    
+        else:
+            query = f"SELECT * FROM books WHERE id={id}"
+            return Database.execute_query(query)
+
+    @staticmethod
+    def reserve_book(origin, id=None):
+
         if Database.user_has_authority(origin, "Reserve Book") is False:
             return False
-        query = ""
+        
+        book = Database.query_book_info(origin)
+
+        if book["Available"]:
+            
+            # Update status of reservation
+            query_template = f"UPDATE books \nSET reserved=1 \nWHERE id={book['ID']}"
+            Database.execute_query(query_template)
+
+            # Update counter for total number of available books
+            query_template = f"UPDATE users \nSET available={book['available']-1} \nWHERE id={origin.id}"
+            Database.execute_query(query_template)
+
+            return True
+        
+        else:
+            print(f"Sorry book is not available.")
+    
+        return False
 
     @staticmethod
     def add_book(origin):
         if Database.user_has_authority(origin, "Add Book") is False:
             return False
         
-        print("adding")
-        query = ""
+        params = [{"Display Name": "Title", "Type": "String"},
+                  {"Display Name": "Author", "Type": "String"},
+                  {"Display Name": "Year", "Type": "Number"},
+                  {"Display Name": "Quantity", "Type": "Number"}]
+        
+        query_template = ""
+        
+        values = Display.prompt_for_query(params, origin, query_template, assemble=False)
+        
+        available = values[3]
+        reserved = 0
+        taken_copies = 0
+        
+        query = f"INSERT INTO books (title, author, year, quantity, available, reserved, taken_copies) VALUES ({values[0]}, {values[1]}, {values[2]}, {values[3]}, {available}, {reserved}, {taken_copies})"
+        
+        if Database.execute_query(query):
+            return True
+    
         return False
     
     @staticmethod
     def add_user(origin):
         if Database.user_has_authority(origin, "Add User") is False:
             return False
-        query = ""
+        
+        params = [{"Display Name": "Name", "Type": "String"},
+                  {"Display Name": "Type", "Type": "String"}]
+        
+        values = Display.prompt_for_query(params, origin, assemble=False)
+
+        books = []
+        reserved = []
+        overdue = []
+        fines = 0
+        max_fines = 100
+        is_allowed = True
+
+        query = f"INSERT INTO users (name, type, books, reserved, overdue, fines, max_fines, is_allowed) VALUES ({values[0]}, {values[1]}, {books}, {reserved}, {overdue}, {fines}, {max_fines}, {is_allowed})"
+
+        if Database.execute_query(query):
+            return True
+        
         return False
